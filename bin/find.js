d7d7adcb53b00e2c3838e02b180ac1f53e251eea
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-01-12 18:04:16 -0500

    feat: add --tags option to print test tag counts table

--- bin/find.js
+++ bin/find.js
@@ -4,13 +4,18 @@ const arg = require('arg')
 const { getSpecs } = require('../src')
 const fs = require('fs')
 const pluralize = require('pluralize')
-const { getTestNames, formatTestList } = require('find-test-names')
+const { getTestNames, formatTestList, countTags } = require('find-test-names')
+const consoleTable = require('console.table')
 
 const args = arg({
   '--names': Boolean,
+  '--tags': Boolean,
 
   // aliases
   '-n': '--names',
+  '--name': '--names',
+  '-t': '--tags',
+  '--tag': '--tags',
 })
 
 const specs = getSpecs()
@@ -21,6 +26,10 @@ if (args['--names']) {
     console.log('')
     let testsN = 0
     let pendingTestsN = 0
+
+    // counts the number of tests for each tag across all specs
+    const tagTestCounts = {}
+
     specs.forEach((filename) => {
       const source = fs.readFileSync(filename, 'utf8')
       const result = getTestNames(source, true)
@@ -43,6 +52,17 @@ if (args['--names']) {
       }
       console.log(formatTestList(result.structure))
       console.log('')
+
+      if (args['--tags']) {
+        const specTagCounts = countTags(result.structure)
+        Object.keys(specTagCounts).forEach((tag) => {
+          if (!(tag in tagTestCounts)) {
+            tagTestCounts[tag] = specTagCounts[tag]
+          } else {
+            tagTestCounts[tag] += specTagCounts[tag]
+          }
+        })
+      }
     })
 
     if (pendingTestsN) {
@@ -60,6 +80,15 @@ if (args['--names']) {
       )
     }
     console.log('')
+
+    if (args['--tags']) {
+      const table = consoleTable.getTable(
+        ['Tag', 'Test count'],
+        Object.entries(tagTestCounts),
+      )
+      console.log(table)
+      console.log('')
+    }
   }
 } else {
   console.log(specs.join(','))
