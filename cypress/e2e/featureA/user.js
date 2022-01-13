cc90520ae9c9a19e5704c75585fdecf5bd74a2f7
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-01-13 16:42:56 -0500

    make the pending test different

--- cypress/e2e/featureA/user.js
+++ cypress/e2e/featureA/user.js
@@ -2,5 +2,7 @@
 
 it('works', { tags: '@user' }, () => {})
 
-// pending test
-it('needs to be written', { tags: '@alpha' })
+// pending test needs to use it.skip
+// since just not having a callback function is not enough
+// https://github.com/cypress-io/cypress/issues/19701
+it.skip('needs to be written', { tags: '@alpha' })
