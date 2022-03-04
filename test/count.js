2facdac4584485aac7f27cb5167ccee74d8af9d6
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-03-04 15:56:13 -0500

    count tests in the suites

--- test/count.js
+++ test/count.js
@@ -1,14 +1,18 @@
 const test = require('ava')
 const input = require('./tagged.json')
-
-/**
- * For each file and each
- */
-function addCounts(json) {}
+const { addCounts } = require('../src/count')
 
 test('counts all tests', (t) => {
-  t.plan(0)
+  t.plan(2)
   const json = JSON.parse(JSON.stringify(input))
   addCounts(json)
-  console.dir(json, { depth: null })
+  // console.dir(json, { depth: null })
+  t.deepEqual(json['cypress/e2e/spec.js'].counts, {
+    tests: 2,
+    pending: 0,
+  })
+  t.deepEqual(json['cypress/e2e/featureA/user.js'].counts, {
+    tests: 2,
+    pending: 1,
+  })
 })
