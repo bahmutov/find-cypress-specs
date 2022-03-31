6b454e350df70771dbb237ee8017482935d46bd5
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2022-03-31 13:46:12 -0400

    chane a spec on main [skip ci]

--- cypress/e2e/spec.js
+++ cypress/e2e/spec.js
@@ -4,6 +4,7 @@ describe('parent suite', { tags: '@main' }, () => {
   it('works well enough', () => {})
 
   context('inner suite', () => {
+    // beautiful test
     it('shows something!', { tags: '@user' }, () => {})
   })
 })
