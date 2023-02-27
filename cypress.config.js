108f9bba117c53382afc2a8fe62701f8c0ab98d9
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2023-02-27 16:43:50 -0500

    print found specs

--- cypress.config.js
+++ cypress.config.js
@@ -1,9 +1,15 @@
 const { defineConfig } = require('cypress')
+const { getSpecs } = require('.')
 
 module.exports = defineConfig({
   fixturesFolder: false,
   e2e: {
-    setupNodeEvents(on, config) {},
+    setupNodeEvents(on, config) {
+      console.log('e2e setupNodeEvents')
+      const specs = getSpecs(config)
+      console.log('found specs')
+      console.log(specs.join(','))
+    },
     supportFile: false,
     specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
     excludeSpecPattern: ['utils.js'],
