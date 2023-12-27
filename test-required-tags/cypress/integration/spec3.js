1b7c3fd3a12466f39dfde118d642c7865887ea61
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2023-12-27 10:24:43 -0500

    more testing

--- test-required-tags/cypress/integration/spec3.js
+++ test-required-tags/cypress/integration/spec3.js
@@ -1,6 +1,12 @@
 // the parent required tags should apply to the tests inside
 // https://github.com/bahmutov/find-cypress-specs/issues/213
 
-describe('parent', { requiredTags: '@parent' }, () => {
-  it('child', () => {})
+// required test tag
+describe('parent1', { requiredTags: '@parent1' }, () => {
+  it('child1', () => {})
+})
+
+// normal test tag
+describe('parent2', { tags: '@parent2' }, () => {
+  it('child2', () => {})
 })
