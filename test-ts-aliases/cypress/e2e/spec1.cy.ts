d62449976bbab6a3a8431a48cf3e5e8aa1afa3d8
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2024-04-30 22:02:13 -0400

    testing ts path aliases

---
+++ test-ts-aliases/cypress/e2e/spec1.cy.ts
@@ -0,0 +1,7 @@
+import { getName } from '@support/utils'
+
+it('has first test', () => {
+  expect(getName()).to.equal('Joe')
+})
+
+it('has second test')
---
+++ test-ts-aliases/cypress/e2e/spec2.cy.ts
@@ -0,0 +1,7 @@
+import { getName } from '@support/utils'
+
+it('has first test')
+
+it('has second test', () => {
+  expect(getName()).to.equal('Joe')
+})
