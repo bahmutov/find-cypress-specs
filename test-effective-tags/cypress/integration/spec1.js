d0664bffa19c656abfc4d5ff61bb8a4eee0dd11c
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2025-01-17 14:49:34 -0500

    add effective tags test folder

---
+++ test-effective-tags/cypress/integration/spec1.js
@@ -0,0 +1,10 @@
+describe('user tests', { tags: '@user' }, () => {
+  it('loads', { tags: '@sanity' }, () => {
+    // something here
+    // this test should have effective tags ['@user', '@sanity']
+  })
+
+  it('works again', () => {
+    // something here
+  })
+})
