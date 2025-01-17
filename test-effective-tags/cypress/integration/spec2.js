3fed67bc62f5301abba7676c503ff0c88cf2f478
Gleb Bahmutov <gleb.bahmutov@gmail.com>
2025-01-17 14:53:22 -0500

    add another spec

---
+++ test-effective-tags/cypress/integration/spec2.js
@@ -0,0 +1,6 @@
+describe('user home tests', () => {
+  // both tags are listed at the test level
+  it('loads', { tags: ['@user', '@sanity'] }, () => {
+    // this test should have effective tags ['@user', '@sanity']
+  })
+})
